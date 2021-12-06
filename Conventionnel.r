f6a76e372bfc046fa1f1a810e88b1ada4dc7e2c0
Flayz96 <95619445+Flayz96@users.noreply.github.com>
2021-12-06 15:58:06 +0100

    Update Conventionnel.r

--- Conventionnel.r
+++ Conventionnel.r
@@ -23,7 +23,7 @@ a <- matrix(c(1020,1100,1170,
               656,778,892,
               212,306,395,
               185,216,245,
-              x,x,x),
+              946,1030,1100),
             3, 5)   #3 colonnes, 5 lignes 
 p <- c(0.33,0.5,0.66)   # probabilités --> quantiles 
 myfit1 <- fitdist(vals = a, probs = p, lower = 0, upper = 1600)
@@ -38,7 +38,7 @@ b <- matrix(c(3.27,3.69,4.09,
               4.11,4.79,5.43,
               4.1,4.5,4.87,
               4.13,4.37,4.6,
-              x,x,x),
+              4.3,4.56,4.81),
             3, 5)   #3 colonnes, 5 lignes 
 q <- c(0.33,0.5,0.66)   # probabilités --> quantiles 
 myfit2 <- fitdist(vals = b, probs = q, lower = 0, upper = 10)
@@ -53,7 +53,7 @@ c <- matrix(c(118,127,136,
               39.8,48.6,56.9,
               69.5,80,89.8,
               36.7,38.9,40.9,
-              x,x,x),
+              53.3,54.9,56.5),
             3, 5)   #3 colonnes, 5 lignes 
 r <- c(0.33,0.5,0.66)   # probabilités --> quantiles 
 myfit3 <- fitdist(vals = c, probs = r, lower = 0, upper = 200)
@@ -68,7 +68,7 @@ d <- matrix(c(1.64,1.8,1.95,
               0.93,1.11,1.28,
               2.23,2.4,2.56,
               1.22,1.36,1.48,
-              x,x,x),
+              1.1,1.14,1.18),
             3, 5)   #3 colonnes, 5 lignes 
 s <- c(0.33,0.5,0.66)   # probabilités --> quantiles 
 myfit4 <- fitdist(vals = d, probs = s, lower = 0, upper = 4)
@@ -87,7 +87,7 @@ d <- matrix(c(796,843,887,
               456,500,541,
               348,400,449,
               625,650,673,
-              x,x,x),
+              517,544,569),
             3, 5)   #3 colonnes, 5 lignes 
 t <- c(0.33,0.5,0.66)   # probabilités --> quantiles 
 myfit5 <- fitdist(vals = d, probs = t, lower = 0, upper = 1000)
@@ -102,7 +102,7 @@ e <- matrix(c(3.06,3.3,3.52,
              1.29,1.63,1.95,
              2.46,2.7,2.92,
              2.0,2.1,2.19,
-             x,x,x),
+             0.65,0.71,0.76),
             3, 5)   #3 colonnes, 5 lignes 
 u <- c(0.33,0.5,0.66)   # probabilités --> quantiles 
 myfit6 <- fitdist(vals = e, probs = u, lower = 0, upper = 6)
@@ -117,7 +117,7 @@ f <- matrix(c(26.7,31.0,35.1,
               19.9,24.5,28.8,
               23.3,27.4,31.2,
               13.4,15.0,16.5,
-              x,x,x),
+              87.5,88.4,89.3),
             3, 5)   #3 colonnes, 2 lignes 
 v <- c(0.33,0.5,0.66)   # probabilités --> quantiles 
 myfit7 <- fitdist(vals = f, probs = v, lower = 0, upper = 100)
@@ -132,7 +132,7 @@ g <- matrix(c(0.63,0.80,0.96,
               0.38,0.41,0.46,
               0.44,0.60,0.75,
               1.08,1.28,1.46,
-              x,x,x),
+              0.27,0.3,0.33),
             3, 5)   #3 colonnes, 5 lignes 
 w <- c(0.33,0.5,0.66)   # probabilités --> quantiles 
 myfit8 <- fitdist(vals = g, probs = w, lower = 0, upper = 4)
