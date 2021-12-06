71d965f170ae44e95ec6474b2b93f452c68e516b
Flayz96 <95619445+Flayz96@users.noreply.github.com>
2021-12-06 15:10:28 +0100

    Update Conventionnel.r

--- Conventionnel.r
+++ Conventionnel.r
@@ -27,7 +27,7 @@ a <- matrix(c(1020,1100,1170,
             3, 5)   #3 colonnes, 5 lignes 
 p <- c(0.33,0.5,0.66)   # probabilités --> quantiles 
 myfit1 <- fitdist(vals = a, probs = p, lower = 0, upper = 1600)
-plotfit(myfit1,lp = T, ql=0.33,qu=0.66, xlab="Rendement en tomates (kg/are)")
+plotfit(myfit1,lp = T, ql=0.33,qu=0.66, xlab="Conventionnel - Rendement en tomates (kg/are)")
 myfit1
 feedback(myfit1)
 # ---------------------
@@ -42,7 +42,7 @@ b <- matrix(c(3.27,3.69,4.09,
             3, 5)   #3 colonnes, 5 lignes 
 q <- c(0.33,0.5,0.66)   # probabilités --> quantiles 
 myfit2 <- fitdist(vals = b, probs = q, lower = 0, upper = 10)
-plotfit(myfit2,lp = T, ql=0.33,qu=0.66, xlab="Prix des tomates (euro/kg)")
+plotfit(myfit2,lp = T, ql=0.33,qu=0.66, xlab="Conventionnel - Prix des tomates (euro/kg)")
 myfit2
 feedback(myfit2)
 # ---------------------
@@ -57,7 +57,7 @@ c <- matrix(c(118,127,136,
             3, 5)   #3 colonnes, 5 lignes 
 r <- c(0.33,0.5,0.66)   # probabilités --> quantiles 
 myfit3 <- fitdist(vals = c, probs = r, lower = 0, upper = 200)
-plotfit(myfit3,lp = T, ql=0.33,qu=0.66, xlab="Temps de travail pour la tomate (h/are/an)")
+plotfit(myfit3,lp = T, ql=0.33,qu=0.66, xlab="Conventionnel - Temps de travail pour la tomate (h/are/an)")
 myfit3
 feedback(myfit3)
 # ---------------------
@@ -72,7 +72,7 @@ d <- matrix(c(1.64,1.8,1.95,
             3, 5)   #3 colonnes, 5 lignes 
 s <- c(0.33,0.5,0.66)   # probabilités --> quantiles 
 myfit4 <- fitdist(vals = d, probs = s, lower = 0, upper = 4)
-plotfit(myfit4,lp = T, ql=0.33,qu=0.66, xlab="Prix d'un plant de tomate (euro/plant)")
+plotfit(myfit4,lp = T, ql=0.33,qu=0.66, xlab="Conventionnel - Prix d'un plant de tomate (euro/plant)")
 myfit4
 feedback(myfit4)
 # ---------------------
@@ -91,7 +91,7 @@ d <- matrix(c(796,843,887,
             3, 5)   #3 colonnes, 5 lignes 
 t <- c(0.33,0.5,0.66)   # probabilités --> quantiles 
 myfit5 <- fitdist(vals = d, probs = t, lower = 0, upper = 1000)
-plotfit(myfit5,lp = T, ql=0.33,qu=0.66, xlab="Rendement en courgette (kg/are)")
+plotfit(myfit5,lp = T, ql=0.33,qu=0.66, xlab="Conventionnel - Rendement en courgette (kg/are)")
 myfit5
 feedback(myfit5)
 # ---------------------
@@ -106,7 +106,7 @@ e <- matrix(c(3.06,3.3,3.52,
             3, 5)   #3 colonnes, 5 lignes 
 u <- c(0.33,0.5,0.66)   # probabilités --> quantiles 
 myfit6 <- fitdist(vals = e, probs = u, lower = 0, upper = 6)
-plotfit(myfit6,lp = T, ql=0.33,qu=0.66, xlab="Prix des courgettes (euro/kg)")
+plotfit(myfit6,lp = T, ql=0.33,qu=0.66, xlab="Conventionnel - Prix des courgettes (euro/kg)")
 myfit6
 feedback(myfit6)
 # ---------------------
@@ -121,7 +121,7 @@ f <- matrix(c(26.7,31.0,35.1,
             3, 5)   #3 colonnes, 2 lignes 
 v <- c(0.33,0.5,0.66)   # probabilités --> quantiles 
 myfit7 <- fitdist(vals = f, probs = v, lower = 0, upper = 100)
-plotfit(myfit7,lp = T, ql=0.33,qu=0.66, xlab="Temps de travail pour la courgette (h/are/an)")
+plotfit(myfit7,lp = T, ql=0.33,qu=0.66, xlab="Conventionnel - Temps de travail pour la courgette (h/are/an)")
 myfit7
 feedback(myfit7)
 # ---------------------
@@ -136,7 +136,7 @@ g <- matrix(c(0.63,0.80,0.96,
             3, 5)   #3 colonnes, 5 lignes 
 w <- c(0.33,0.5,0.66)   # probabilités --> quantiles 
 myfit8 <- fitdist(vals = g, probs = w, lower = 0, upper = 4)
-plotfit(myfit8,lp = T, ql=0.33,qu=0.66, xlab="Prix d'un plant de courgette (euro/plant)")
+plotfit(myfit8,lp = T, ql=0.33,qu=0.66, xlab="Conventionnel - Prix d'un plant de courgette (euro/plant)")
 myfit8
 feedback(myfit8)
 # ---------------------
